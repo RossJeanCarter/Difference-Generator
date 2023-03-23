d96fa1b9e69a21145c36537fe20a1a256bea3bd5
Ross Carter <indovinachi99@gmail.com>
2023-03-23 19:48:43 +0300

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-
-import { Command } from 'commander';
-
-const program = new Command();
+import { program } from 'commander';
+import gendiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0 ')
-  .option('-f, --format <type>', 'output format')
+  .version('1.0.0.')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action(
+    (filepath1, filepath2) => {
+      gendiff(filepath1, filepath2);
+    }
+  )
+
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+export default (filepath1, filepath2) => {
+  const data1 = JSON.parse(readFileSync(filepath1, 'utf8'));
+  const data2 = JSON.parse(readFileSync(filepath2, 'utf8'));
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keysAll = _.union(keys1, keys2);
+  const sortedKeys = _.orderBy((keysAll));
+
+  const comparePaths = sortedKeys.reduce((acc, key) => {
+    if (keys1.includes(key) && keys2.includes(key)) {
+      if (data1[key] === data2[key]) {
+        acc += `\n    ${key}: ${data1[key]}`; //
+      } else {
+        acc += `\n  - ${key}: ${data1[key]}`;//-
+        acc += `\n  + ${key}: ${data2[key]}`;//+
+      }
+    } else if (keys1.includes(key)) {
+      acc += `\n  - ${key}: ${data1[key]}`;//-
+    } else if (keys2.includes(key)) {
+      acc += `\n  + ${key}: ${data2[key]}`;//+
+    }
+    return acc;
+  }, '{');
+  const result = `${comparePaths}\n}`;
+  console.log(result);
+  return `${comparePaths}\n}`;
+};
+
+
