d23170588d0123eddc0704a443073ece590cbba2
Ross Carter <indovinachi99@gmail.com>
2023-06-26 13:57:50 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,11 @@
-import { describe, test, expect } from '@jest/globals';
 import { readFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import parser from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
 const jsonFile1 = getFixturePath('file1.json');
 const jsonFile2 = getFixturePath('file2.json');
